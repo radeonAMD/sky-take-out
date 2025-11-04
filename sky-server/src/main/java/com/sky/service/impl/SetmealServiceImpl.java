8a96a75a6e8d8c7fb2dda0736c5db964a6c1819c
zzzlnO <1874317291@qq.com>
2025-11-05 00:03:55 +0800

    商品浏览业务功能业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -6,11 +6,14 @@ import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
+import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
 import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -125,4 +128,22 @@ public class SetmealServiceImpl implements SetmealService {
         //批量插入菜品
         setmealDishMapper.insertBatch(setmealDishes);
     }
+
+    /**
+     * 套餐业务实现
+     */
+
+        public List<Setmeal> list(Setmeal setmeal) {
+            List<Setmeal> list = setmealMapper.list(setmeal);
+            return list;
+        }
+
+        /**
+         * 根据id查询菜品选项
+         * @param id
+         * @return
+         */
+        public List<DishItemVO> getDishItemById(Long id) {
+            return setmealMapper.getDishItemBySetmealId(id);
+        }
 }
