8d038157e67b82034a77c7245f10f22433cc19b4
zzzlnO <1874317291@qq.com>
2025-11-10 19:13:51 +0800

    查看历史订单功能代码开发

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,8 +1,12 @@
 package com.sky.service;
 
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +14,24 @@ public interface OrderService {
 
 
     OrderSubmitVO submit(OrdersSubmitDTO ordersSubmitDTO);
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception;
+
+    /**
+     * 支付成功，修改订单状态
+     *
+     * @param outTradeNo
+     */
+    void paySuccess(String outTradeNo);
+
+    OrderVO orderDetail(Long id);
+
+    PageResult pageQuery4User(int page, int pageSize, Integer status);
+
 }
